4b4aab7a6b03c5a5d9b7bba86c1b2f8cf33f9dde
Tolera Degefa <100357038+todaycu@users.noreply.github.com>
2023-01-12 15:23:55 +0300

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-    ORDER BY `lifespan` DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT DISTINCT `band_name`, IFNULL(`split`, 2022) - `formed` as `lifespan`
+FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+ORDER BY `lifespan` DESC;
